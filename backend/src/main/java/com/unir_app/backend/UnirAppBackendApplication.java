8c5d2021c15c92aedc2a9e963298e4e684c03565
jonanct <jonanct@gmail.com>
2025-02-26 22:50:45 -0800

    backend start
    backend start

---
+++ backend/src/main/java/com/unir_app/backend/UnirAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.unir_app.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnirAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnirAppBackendApplication.class, args);
+	}
+
+}
